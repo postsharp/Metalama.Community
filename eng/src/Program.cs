0cd80efba3312075fc4d67c295595d37ff5743ca
Jan Hlavac <jan@postsharp.net>
2022-12-14 12:26:13 +0100

    Add MergePublisher.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
+using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
+using System.Linq;
 
 var product = new Product( Dependencies.MetalamaCommunity )
 {
@@ -19,7 +22,12 @@ var product = new Product( Dependencies.MetalamaCommunity )
         "Metalama.Community.Costura.Redist.$(PackageVersion).nupkg",
         "Metalama.Community.Virtuosity.$(PackageVersion).nupkg",
         "Metalama.Community.Virtuosity.Redist.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama }
+    Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
+    Configurations = Product.DefaultConfigurations
+        .WithValue( BuildConfiguration.Public, new BuildConfigurationInfo(
+            MSBuildName: "Release",
+            RequiresSigning: true,
+            PublicPublishers: Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() ) )
 };
 
 var commandApp = new CommandApp();
