92919440da5a0e08363b24e61939c4c25ac2d22c
Antonín Procházka <antonin@postsharp.net>
2023-04-27 14:18:40 +0200

    Release 2023.1 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,13 +1,10 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
-using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
-using PostSharp.Engineering.BuildTools.Build.Publishers;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Model;
 using Spectre.Console.Cli;
-using System.Linq;
 
 var product = new Product( Dependencies.MetalamaCommunity )
 {
@@ -23,11 +20,13 @@ var product = new Product( Dependencies.MetalamaCommunity )
         "Metalama.Community.Virtuosity.$(PackageVersion).nupkg",
         "Metalama.Community.Virtuosity.Redist.$(PackageVersion).nupkg" ),
     Dependencies = new[] { Dependencies.PostSharpEngineering, Dependencies.Metalama },
-    Configurations = Product.DefaultConfigurations
-        .WithValue( BuildConfiguration.Public, new BuildConfigurationInfo(
-            MSBuildName: "Release",
-            RequiresSigning: true,
-            PublicPublishers: Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() ) )
+    
+    // MergePublisher disabled for 2023.1.
+    // Configurations = Product.DefaultConfigurations
+    //     .WithValue( BuildConfiguration.Public, new BuildConfigurationInfo(
+    //         MSBuildName: "Release",
+    //         RequiresSigning: true,
+    //         PublicPublishers: Product.DefaultPublicPublishers.Add( new MergePublisher() ).ToArray() ) )
 };
 
 var commandApp = new CommandApp();
