2a523eade095ce698f9e250eeaad3965e8577b6f
Gael Fraiteur <gael@postsharp.net>
2026-02-01 09:56:23 +0100

    Eng: using preferred .NET versions.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
@@ -8,7 +9,7 @@ using PostSharp.Engineering.BuildTools.Docker;
 using System;
 using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2026_0;
 
-const string dotNetSdkVersion = "10.0.100";
+const string dotNetSdkVersion = PreferredVersions.DotNetSdk.V_10_0;
 
 var product = new Product( MetalamaDependencies.MetalamaCommunity )
 {
@@ -17,7 +18,7 @@ var product = new Product( MetalamaDependencies.MetalamaCommunity )
         Components =
         [
             new DotNetComponent( dotNetSdkVersion, DotNetComponentKind.Sdk ),
-            new DotNetComponent( "9.0.305", DotNetComponentKind.Sdk ),
+            new DotNetComponent( PreferredVersions.DotNetSdk.V_9_0, DotNetComponentKind.Sdk ),
         ]
     },
     GenerateNuGetConfig = true,
