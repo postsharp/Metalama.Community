8b8ac913ab005f5019c009b5c438b8b3f6403750
Gael Fraiteur <gael@postsharp.net>
2026-07-24 19:30:46 +0200

    Suppress IDE0051 in the record test, as the other tests do
    CI enforces code style, so the private members that exist precisely to
    show they are not virtualized were reported as unused and failed the
    build. Class.cs already disables IDE0051 for the same reason.

--- src/Metalama.Community.Virtuosity/Metalama.Community.Virtuosity.UnitTests/Record.cs
+++ src/Metalama.Community.Virtuosity/Metalama.Community.Virtuosity.UnitTests/Record.cs
@@ -1,6 +1,7 @@
 // Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 #pragma warning disable CA1822 // Mark members as static
+#pragma warning disable IDE0051 // Remove unused private members - the private members are here to show that they are not virtualized.
 
 namespace Metalama.Community.Virtuosity.Tests.Record
 {
